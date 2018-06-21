12516cc28c0c056be7145371e34d3c1e38cb2020
DevilAslan <h_duke@163.com>
2018-06-21 09:55:35 +0800

    0.0

---
+++ D2.Designs/code/trunk/DemoCore/demo.cn.umbrella.commons.utils/src/main/java/cn/umbrella/commons/enums/EnumTest.java
@@ -0,0 +1,11 @@
+package cn.umbrella.commons.enums;
+
+import cn.umbrella.commons.enums.CommonEnums.Boolflag;
+
+public class EnumTest {
+
+	public static void main(String[] args) {
+		System.out.println(Boolflag.FALSE);
+	}
+
+}
