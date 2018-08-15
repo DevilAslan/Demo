7d441a0c56cede311f918c93f11486201a0e8080
shawMoriarty <1141883643@qq.com>
2018-08-15 16:11:52 +0800

    0.0

--- D2.Designs/code/trunk/DemoCore/demo.cn.umbrella.commons.util/src/main/java/cn/umbrella/commons/enums/ICommonEnum.java
+++ D2.Designs/code/trunk/DemoCore/demo.cn.umbrella.commons.util/src/main/java/cn/umbrella/commons/enums/ICommonEnum.java
@@ -1,33 +1,16 @@
 package cn.umbrella.commons.enums;
 
 /**
- * 一般键值型枚举的接口，规范 获取键和值的方式<br>
  * 
- * <br>请尽量使用IGDCommonEnum接口
- * 
- * @author
- * @createDate 
- * 
+ * 一般键值型枚举接口，规范 获取键和值的方式  
+ *
+ * @ClassName: IGDCommonEnum  
+ * @author zhou.xy
+ * @date
+ *  
+ * @param <T>
  */
-@Deprecated
-public interface ICommonEnum  {
+public interface IGDCommonEnum<T> {
+	public T getValue();
 	public String getName();
-	public int getValue();
 }
-
-/**
- * 
- * 通过值查找到名称， 通常用在 列表或者详情页面获取值
- * 由于枚举不能继承， 接口不能声明静态方法
- * 
- */
-/*
-public static String getNameByValue(int ordinal) {
-	for(ICommonEnum ce:values()){
-		if(ce.getValue()==ordinal){
-			return ce.getName();
-		}
-	}
-	return null;
-}
-*/
